dfac72083acfefe7267fcfd3fddf25ba46da9b09
Rodrigo Nogueira <rdg6design@gmail.com>
2020-06-02 20:46:12 -0300

    react-hooks

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
-function App() {
+const useFetch =(url)=>{
+  const [data, setData] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  //similar to componentDidMount and componentDidUpdate
+  useEffect(()=>{
+    //Update the document title using the browser API
+    // document.title = `You clicked ${count} times`
+    (async function() {
+      try{
+        const response = await fetch(url)
+        const data = await response.json()
+        // const item = data.results[0]
+        const [item] = data.results
+        setData(item) 
+        setLoading(false)
+      } catch (e) {
+        console.error( "404 Not Found");
+      }
+   })();
+  }, [])
+
+  return {data, loading}
+}
+
+const App = () => {
+  
+  // const [count, setCount] = useState(0)
+  const {data, loading} = useFetch('https://api.randomuser.me')
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    {/* <p>You clicked {count} times</p> */}
+    {/* <button onClick={()=>{setCount(count + 1)}}>Click me</button> */}
+      {loading ? <div>...loading</div> : <div>{data.name.first}</div>}
     </div>
   );
 }
